91ed9494e4d9082247507e722812484793369f6a
Roy Okuwaki <register@tinybeans.net>
2018-11-14 10:15:08 +1300

    add: Added charset to header

--- search/search.php
+++ search/search.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 
 require_once(__DIR__ . '/bpSearch/BPSearch.php');
 $config = [
